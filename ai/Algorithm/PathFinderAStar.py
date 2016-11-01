12b3c9fd86170f04271bf5625611906067ad816a
JeromeCossette <jerome.cossette@hotmail.fr>
2016-10-31 22:39:04 -0400

    debut algo a*

--- ai/Algorithm/PathFinderAStar.py
+++ ai/Algorithm/PathFinderAStar.py
@@ -1,13 +1,48 @@
 """
 Class  cooperative A*
 """
+from queue import PriorityQueue
+
+
+class SimpleGraph:
+    def __init__(self):
+        self.edges = {}
+
+    # return a list
+    def neighbors(self, id):
+        return self.edges[id]
 
 
 class PathFinderAStar:
-    node = 0
+    def __init__(self, start, goal):
+        graph = SimpleGraph
+        self.a_star_search(graph, start, goal)
 
-    def __init__(self, test):
-        self.node = test
+    def heuristic(self, a, b):
+        (x1, y1) = a
+        (x2, y2) = b
+        return abs(x1 - x2) + abs(y1 - y2)
 
-    def test(self):
-        self.node = 4
+    def a_star_search(self, graph, start, goal):
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[start] = None
+        cost_so_far[start] = 0
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == goal:
+                break
+
+            for next in graph.neighbors(current):
+                new_cost = cost_so_far[current] + graph.cost(current, next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + self.heuristic(goal, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+
+        return came_from, cost_so_far
